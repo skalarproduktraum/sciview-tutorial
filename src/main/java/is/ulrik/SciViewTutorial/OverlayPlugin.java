c99319982668b12abc47a8db043b4944d70dad54
Ulrik Günther <ulrik@42degreesoffreedom.com>
2018-06-07 00:52:29 +0200

    OverlayPlugin: Add marching cube'd meshes

--- src/main/java/is/ulrik/SciViewTutorial/OverlayPlugin.java
+++ src/main/java/is/ulrik/SciViewTutorial/OverlayPlugin.java
@@ -1,25 +1,27 @@
 package is.ulrik.SciViewTutorial;
 
 import cleargl.GLVector;
-import io.scif.SCIFIOService;
-import net.imagej.ImageJService;
+import graphics.scenery.Node;
+import net.imagej.mesh.Mesh;
+import net.imagej.ops.OpService;
+import net.imglib2.img.Img;
 import net.imglib2.type.NativeType;
+import net.imglib2.type.logic.BitType;
 import net.imglib2.type.numeric.RealType;
-import org.scijava.Context;
+import net.imglib2.type.numeric.integer.UnsignedByteType;
 import org.scijava.InstantiableException;
 import org.scijava.command.Command;
 import org.scijava.command.CommandInfo;
 import org.scijava.command.CommandService;
 import org.scijava.plugin.Menu;
+import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
-import org.scijava.service.SciJavaService;
 import org.scijava.ui.UIService;
 import sc.iview.SciView;
-import sc.iview.SciViewService;
 import sc.iview.commands.demo.GameOfLife3D;
 
 /**
- * Plugin that opens the currently active image in SciView, and plays it if it is a time lapse.
+ * Plugin that runs the game of life demo, and adds marching cube'd meshes of the volume data.
  *
  * @author Ulrik Günther <hello@ulrik.is>
  */
@@ -28,40 +30,55 @@ import sc.iview.commands.demo.GameOfLife3D;
         @Menu( label = "Mesh Overlay" ) }, description = "Demos overlaying meshes and volumes", headless = false, type = Command.class )
 public class OverlayPlugin< T extends RealType< T > & NativeType< T >> implements Command {
 
-  private SciViewService sciViewService;
+  @Parameter
   private SciView sciView;
+
+  @Parameter
+  private OpService ops;
+
+  @Parameter
+  private UIService ui;
+
+  @Parameter
+  private CommandService cmds;
 
   /**
    * Runs the HelloWorldPlugin.
    */
   @Override
   public void run() {
-    Context context = new Context( ImageJService.class, SciJavaService.class, SCIFIOService.class );
-
-    UIService ui = context.service( UIService.class );
     if( !ui.isVisible() ) ui.showUI();
 
-    sciViewService = context.service( SciViewService.class );
-    sciView = sciViewService.getOrCreateActiveSciView();
-
-    sciView.getCamera().setPosition( new GLVector( 0.0f, 0.0f, 5.0f ) );
+    sciView.getCamera().setPosition( new GLVector( 10.0f, 0.0f, 15.0f ) );
     sciView.getCamera().setTargeted( true );
     sciView.getCamera().setTarget( new GLVector( 0, 0, 0 ) );
     sciView.getCamera().setDirty( true );
     sciView.getCamera().setNeedsUpdate( true );
 
-
     try {
-      CommandService cmds = context.service(CommandService.class);
       CommandInfo cmdInfo = cmds.getCommand(sc.iview.commands.demo.GameOfLife3D.class);
       GameOfLife3D gol = (GameOfLife3D)cmdInfo.createInstance();
 
-      gol.setContext(context);
+      gol.setContext(ui.context());
       gol.setInput("sciView", sciView);
       gol.run();
 
-      gol.getVolume().setPosition(new GLVector(0.0f, 0.0f, 0.0f));
-      gol.getVolume().setScale(new GLVector(1.0f, 1.0f, 1.0f));
+      int i = 0;
+      while(i < 20) {
+        Img<BitType> bitImg = (Img<BitType>) ops.threshold().apply(gol.getImg(), new UnsignedByteType(128));
+        Mesh mesh = ops.geom().marchingCubes(bitImg);
+        Node n = sciView.addMesh(mesh);
+
+        i++;
+        try {
+          Thread.sleep(5000);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+
+        n.setVisible(false);
+        gol.iterate();
+      }
 
     } catch (InstantiableException e) {
       e.printStackTrace();
