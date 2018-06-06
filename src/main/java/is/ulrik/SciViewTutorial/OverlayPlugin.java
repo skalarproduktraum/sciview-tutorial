18021e2378f7f76c0641b8311aebb81579f40857
Ulrik Günther <ulrik@42degreesoffreedom.com>
2018-06-07 00:19:04 +0200

    OverlayPlugin: run GameOfLife3D programmatically

--- src/main/java/is/ulrik/SciViewTutorial/OverlayPlugin.java
+++ src/main/java/is/ulrik/SciViewTutorial/OverlayPlugin.java
@@ -1,23 +1,22 @@
 package is.ulrik.SciViewTutorial;
 
 import cleargl.GLVector;
-import graphics.scenery.Node;
 import io.scif.SCIFIOService;
 import net.imagej.ImageJService;
-import net.imagej.ImgPlus;
-import net.imagej.display.DatasetView;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.RealType;
 import org.scijava.Context;
+import org.scijava.InstantiableException;
 import org.scijava.command.Command;
+import org.scijava.command.CommandInfo;
 import org.scijava.command.CommandService;
 import org.scijava.plugin.Menu;
-import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 import org.scijava.service.SciJavaService;
 import org.scijava.ui.UIService;
 import sc.iview.SciView;
 import sc.iview.SciViewService;
+import sc.iview.commands.demo.GameOfLife3D;
 
 /**
  * Plugin that opens the currently active image in SciView, and plays it if it is a time lapse.
@@ -29,10 +28,6 @@ import sc.iview.SciViewService;
         @Menu( label = "Mesh Overlay" ) }, description = "Demos overlaying meshes and volumes", headless = false, type = Command.class )
 public class OverlayPlugin< T extends RealType< T > & NativeType< T >> implements Command {
 
-  @Parameter( label = "3D ImgPlus to be shown." )
-  private DatasetView datasetView;
-
-  private ImgPlus< T > imgPlus;
   private SciViewService sciViewService;
   private SciView sciView;
 
@@ -41,8 +36,6 @@ public class OverlayPlugin< T extends RealType< T > & NativeType< T >> implement
    */
   @Override
   public void run() {
-    imgPlus = (ImgPlus< T >) datasetView.getData().getImgPlus();
-
     Context context = new Context( ImageJService.class, SciJavaService.class, SCIFIOService.class );
 
     UIService ui = context.service( UIService.class );
@@ -57,12 +50,22 @@ public class OverlayPlugin< T extends RealType< T > & NativeType< T >> implement
     sciView.getCamera().setDirty( true );
     sciView.getCamera().setNeedsUpdate( true );
 
-    Node v = sciView.addVolume(datasetView.getData());
-    v.setPosition(new GLVector(0.0f, 0.0f, 0.0f));
-    v.setScale(new GLVector(1.0f, 1.0f, 1.0f));
 
-    CommandService cmd = context.service(CommandService.class);
-    cmd.run(sc.iview.commands.demo.GameOfLife3D.class, true);
+    try {
+      CommandService cmds = context.service(CommandService.class);
+      CommandInfo cmdInfo = cmds.getCommand(sc.iview.commands.demo.GameOfLife3D.class);
+      GameOfLife3D gol = (GameOfLife3D)cmdInfo.createInstance();
+
+      gol.setContext(context);
+      gol.setInput("sciView", sciView);
+      gol.run();
+
+      gol.getVolume().setPosition(new GLVector(0.0f, 0.0f, 0.0f));
+      gol.getVolume().setScale(new GLVector(1.0f, 1.0f, 1.0f));
+
+    } catch (InstantiableException e) {
+      e.printStackTrace();
+    }
   }
 }
 
